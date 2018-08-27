19ae9050f848f1dde737ccb42c959db1d7bcbf86
Miguel <mfrasquetherraiz@gmail.com>
2018-08-27 11:12:29 +0200

    report again

--- Ressspi.py
+++ Ressspi.py
@@ -1168,7 +1168,7 @@ def ressspiSIM(ressspiReg,data_reg,inputsDjango,inputFile,printReport,plots,imag
 
         if ressspiReg==-2: 
             data=""
-        #template_vars=reportOutput(ressspiReg,reg,reportsVar,inputs,data,printReport,pk,version,os.path.dirname(os.path.dirname(__file__))+'/ressspi',os.path.dirname(os.path.dirname(__file__)))
+        template_vars=reportOutput(ressspiReg,reg,reportsVar,inputs,data,printReport,pk,version,os.path.dirname(os.path.dirname(__file__))+'/ressspi',os.path.dirname(os.path.dirname(__file__)))
     else:
         template_vars={}
         reportsVar={}
