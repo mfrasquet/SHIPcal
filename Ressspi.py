6a80d98aca78ac64e8d4c912dbfe64b4f012c178
Miguel <mfrasquetherraiz@gmail.com>
2018-10-17 16:13:15 +0200

    call of function IAM

--- Ressspi.py
+++ Ressspi.py
@@ -582,7 +582,7 @@ def ressspiSIM(ressspiReg,inputsDjango,plots,imageQlty,confReport,modificators,d
     for i in range(0,steps_sim):
    
         theta_transv_rad[i],theta_i_rad[i]=theta_IAMs(SUN_AZ[i],SUN_ELV[i],beta,orient_az_rad)
-#        theta_transv_rad[i],theta_i_rad[i]=theta_IAMs_v2(SUN_AZ[i],SUN_ELV[i],beta,orient_az_rad,roll)
+#        theta_transv_deg[i],abs(theta_i_deg[i])=theta_IAMs_v2(SUN_AZ[i],SUN_ELV[i],beta,orient_az_rad,roll)
         
         #Cálculo del IAM long y transv
         theta_i_deg[i]=theta_i_rad[i]*180/np.pi
@@ -590,6 +590,10 @@ def ressspiSIM(ressspiReg,inputsDjango,plots,imageQlty,confReport,modificators,d
         
         [IAM_long[i]]=IAM_calc(theta_i_deg[i],0,IAMfile_loc) #Longitudinal
         [IAM_t[i]]=IAM_calc(theta_transv_deg[i],1,IAMfile_loc) #Transversal
+        
+
+#        IAM[i]=optic_efficiency_N(theta_transv_deg[i],theta_i_deg[i],n_coll_loop) #(theta_transv_deg[i],theta_i_deg[i],n_coll_loop):
+        
         
     #        if IAM_long[i]>1:
     #            IAM_long[i]=1
