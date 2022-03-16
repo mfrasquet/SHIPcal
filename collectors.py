5151ced444b87d23f078ab10ef7b3da57957b437
miguel <miguel.frasquet@solatom.com>
2022-03-16 16:49:01 +0100

    test1

--- collectors.py
+++ collectors.py
@@ -8,4 +8,4 @@ class Collector(Element):
     rho_optic = 0.5883 # [-]
     nu_1 = 0.0783 # [W/(m^2 K)]
     nu_2 = 0.003163 # [W/(m^2 K^2)]
-    mdot_test = 0.0308 # [kg/sm2]
+    mdot_test = 0.0308 # [kg/sm2] 
