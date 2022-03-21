c9934c648ba15fbfbbf84ddab426b717f70bd3be
jaarpa <ja.arpa97@gmail.com>
2022-03-21 04:42:12 -0600

    Update conf for linter and test for new files structure

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -6,6 +6,6 @@ from shipcal import Weather
 
 def test_read_tmy3():
     """ Check that tmy3 format is bein read correctly """
-    sevilla_tmy3_loc = "/home/jaarpa/aProjects/solatom/SHIPcal/shipcal/weather/data/Sevilla.csv"
+    sevilla_tmy3_loc = "src/shipcal/weather/data/Sevilla.csv"
     sevilla = Weather(sevilla_tmy3_loc)
     assert sevilla.lat == 37.410
