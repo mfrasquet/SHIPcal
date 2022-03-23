57216687e98d8ba80943f7f2a974fd707a7e6522
Miguel Frasquet <mfrasquetherraiz@gmail.com>
2022-03-23 17:15:32 +0100

    init optics

--- src/shipcal/collectors/collector.py
+++ src/shipcal/collectors/collector.py
@@ -1,5 +1,19 @@
+"""
+This file contains the classes that define the collector in
+shipcal
+"""
+
 from shipcal.elements import Element
 
+class FresnelOptics():
+    """
+    Class for the optics in Linear Fresnel collectors.
+    eff_opt_norm => Optical efficiency at normal incidence in % [float]
+    iam_file => IAM file for the collector [csv]
+    """
+    def __init__(self,eff_opt_norm,iam_file):
+        self.eff_opt_norm = eff_opt_norm
+        self.iam_file = iam_file
 
 class Collector(Element):
     """
@@ -10,3 +24,6 @@ class Collector(Element):
     nu_1 = 0.0783  # [W/(m^2 K)]
     nu_2 = 0.003163  # [W/(m^2 K^2)]
     mdot_test = 0.0308  # [kg/sm2]
+
+
+optic = FresnelOptics(67.56,45)
